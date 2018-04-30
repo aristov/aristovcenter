9f73a8df063565ce2ffd2aaf0d46da32c7013591
aristov <vv.aristov@gmail.com>
2018-05-01 00:53:29 +0300

    Обработка тач-событий, тюнинг стилей

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 {
+    const DELAY = 5000
+    const TOUCH_DISTANCE_THRESHOLD = 150
     const body = document.body
     const main = document.querySelector('main')
     let timerId
-    body.onkeydown = event => {
-        const key = event.key
-        if(key === 'ArrowRight' || key === ' ') {
-            next()
-            clearInterval(timerId)
-        }
-        else if(key === 'ArrowLeft') {
-            prev()
-            clearInterval(timerId)
-        }
-    }
-    main.onclick = event => {
-        next()
-        clearInterval(timerId)
-    }
     function next() {
         main.append(main.firstElementChild)
         changeColor()
@@ -30,11 +17,49 @@
         body.style.backgroundColor = bg || ''
         body.style.color = color || ''
     }
-    function intervalSwitch() {
+    function startTimer() {
         timerId = setTimeout(() => {
             next()
-            intervalSwitch()
-        }, 5000)
+            startTimer()
+        }, DELAY)
     }
-    intervalSwitch()
+    function stopTimer() {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        }
+    }
+    body.onkeydown = event => {
+        const key = event.key
+        if(key === 'ArrowRight' || key === ' ') {
+            next()
+            stopTimer()
+        }
+        else if(key === 'ArrowLeft') {
+            prev()
+            stopTimer()
+        }
+    }
+    main.onclick = event => {
+        next()
+        stopTimer()
+    }
+    body.ontouchstart = event => {
+        const changedTouches = event.changedTouches
+        const startX = changedTouches[0].clientX
+        body.ontouchend = event => {
+            const changedTouches = event.changedTouches
+            const endX = changedTouches[0].clientX
+            if(endX > startX + TOUCH_DISTANCE_THRESHOLD) {
+                prev()
+                stopTimer()
+            }
+            else if(startX > endX + TOUCH_DISTANCE_THRESHOLD) {
+                next()
+                stopTimer()
+            }
+            body.ontouchend = null
+        }
+    }
+    startTimer()
 }
