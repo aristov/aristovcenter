9159a47251cb09a7a612c4ef55f0f0f82854c174
aristov <vv.aristov@gmail.com>
2018-05-18 23:53:35 +0300

    Настраиваем Webpack, Babel, PostCSS, UglifyJS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'ariamodule/lib/aria/html'
 import { Gallery } from './gallery'
 import { MenuButton } from './menubutton'
+import './style/index.css'
 
 MenuButton.init('#menubutton')
 Gallery.init('#gallery')
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,60 @@
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssoPlugin = require('csso-webpack-plugin').default
+
+const NODE_ENV = process.env.NODE_ENV
+
+const CSS_EXT_RE = /\.css$/
+const JS_EXT_RE = /\.js$/
+
+const rules = [
+    {
+        test : CSS_EXT_RE,
+        use : [
+            MiniCssExtractPlugin.loader,
+            {
+                loader : 'css-loader',
+                options : {
+                    importLoaders : 1,
+                    url : false
+                }
+            },
+            'postcss-loader'
+        ]
+    },
+    {
+        test : /\.svg$/,
+        use : 'raw-loader'
+    }
+]
+const plugins = [
+    new MiniCssExtractPlugin({ filename : 'build.[name].css' })
+]
+
+if(NODE_ENV === 'production') {
+    rules.push({
+        test : JS_EXT_RE,
+        use : { loader : 'babel-loader' }
+    })
+    plugins.push(new UglifyJsPlugin({
+        uglifyOptions : {
+            keep_fnames : true,
+            keep_classnames : true
+        }
+    }))
+    plugins.push(new CssoPlugin)
+}
 
 module.exports = {
     mode : 'none',
-    entry : './src/index.js',
+    entry : {
+        index : './src/index.js'
+    },
     output : {
         path : path.join(__dirname, 'build'),
-        filename : 'build.index.js'
+        filename : 'build.[name].js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.svg$/,
-                use: 'raw-loader'
-            }
-        ]
-    }
+    module : { rules },
+    plugins
 }
